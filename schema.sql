16e164f752639a0798ed3f2b43eaebc045f060c5
Denis Carriere <carriere.denis@gmail.com>
2024-02-13 22:10:44 -0500

    update SQL schema

--- schema.sql
+++ schema.sql
@@ -1,47 +1,77 @@
+-- Table for transfers --
 CREATE TABLE IF NOT EXISTS transfers (
+    -- trace information
     trx_id String,
     action_index UInt32,
+
+    -- contract & scope --
     contract FixedString(12),
     action String,
     symcode String,
+
+    -- data payload --
     from FixedString(12),
     to FixedString(12),
-    memo String,
     quantity String,
-    amount Int64,
+    memo String,
+
+    -- extras --
     precision UInt32,
+    amount Int64,
     value Float64,
 )
-ENGINE = MergeTree()
-ORDER BY (timestamp, block_number, chain); # Created by Clickhouse Sink
+ENGINE = ReplacingMergeTree()
+-- primary key = trx_id + action_index --
+PRIMARY KEY (id)
+ORDER BY (id)
 
+-- Table for accounts --
 CREATE TABLE IF NOT EXISTS accounts (
+    -- trace information --
     trx_id String,
     action_index UInt32,
+
+    -- contract & scope --
     contract FixedString(12),
     symcode String,
-    amount Int64,
-    precision UInt32,
-    value Float64,
+
+    -- data payload --
     account FixedString(12),
     balance String,
     balance_delta Int64,
-)
-ENGINE = MergeTree()
-ORDER BY (timestamp, block_number, chain); # Created by Clickhouse Sink
 
+    -- extras --
+    precision UInt32,
+    amount Int64,
+    value Float64,
+)
+ENGINE = ReplacingMergeTree()
+-- primary key = trx_id + action_index --
+PRIMARY KEY (id)
+ORDER BY (id)
+
+-- Table for stats --
 CREATE TABLE IF NOT EXISTS stats (
+    -- trace information --
     trx_id String,
     action_index UInt32,
+
+    -- contract & scope --
     contract FixedString(12),
     symcode String,
+
+    -- data payload --
     issuer FixedString(12),
     max_supply String,
     supply String,
     supply_delta Int64,
+
+    -- extras --
     precision UInt32,
     amount Int64,
     value Float64,
 )
-ENGINE = MergeTree()
-ORDER BY (timestamp, block_number, chain) # Created by Clickhouse Sink
+ENGINE = ReplacingMergeTree()
+-- primary key = trx_id + action_index --
+PRIMARY KEY (id)
+ORDER BY (id)
